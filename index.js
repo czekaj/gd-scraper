2311d758295f6c767b5b52746ae9fa64f4d5165c
Lucas Czekaj <lukasz@czekaj.us>
2018-09-26 21:17:48 -0700

    Returns proper JSON now

--- index.js
+++ index.js
@@ -13,39 +13,45 @@ var driver = new webdriver.Builder()
   .setChromeOptions(options)
   .build()
 
+var reviews = []
+
 driver.get(url).then(async () => {
-  var reviews = await driver.findElements({
+  var reviewEls = await driver.findElements({
     className: 'empReview'
   })
-  reviews.forEach(async (review) => {
-    var getFieldByClass = async (classNameValue) => {
-      try {
-        var field = await review.findElement({
-          className: classNameValue
-        })
-      } catch (e) {
-        return ''
+  var iterateOverReviews = async () => {
+    await Promise.all(reviewEls.map(async (reviewEl) => {
+      var getFieldByClass = async (classNameValue) => {
+        try {
+          var field = await reviewEl.findElement({
+            className: classNameValue
+          })
+        } catch (e) {
+          return ''
+        }
+        return field.getText()
       }
-      return field.getText()
-    }
-    var date = await review.findElement({
-      tagName: 'time'
-    })
-    date = await date.getText()
-    var stars = await review.findElement({
-      className: 'value-title'
-    })
-    stars = await stars.getAttribute('title')
-    stars = '*'.repeat(stars)
-    var reviewObj = {
-      date,
-      summary: await getFieldByClass('summary'),
-      stars,
-      author: await getFieldByClass('authorInfo'),
-      pros: await getFieldByClass('pros'),
-      cons: await getFieldByClass('cons'),
-      advice: await getFieldByClass('adviceMgmt')
-    }
-    console.log(JSON.stringify({ review: reviewObj }, null, 2))
-  })
+      var date = await reviewEl.findElement({
+        tagName: 'time'
+      })
+      date = await date.getText()
+      var stars = await reviewEl.findElement({
+        className: 'value-title'
+      })
+      stars = await stars.getAttribute('title')
+      stars = '*'.repeat(stars)
+      var review = {
+        date,
+        summary: await getFieldByClass('summary'),
+        stars,
+        author: await getFieldByClass('authorInfo'),
+        pros: await getFieldByClass('pros'),
+        cons: await getFieldByClass('cons'),
+        advice: await getFieldByClass('adviceMgmt')
+      }
+      reviews.push({ review })
+    }))
+    console.log(JSON.stringify(reviews, null, 2))
+  }
+  iterateOverReviews()
 })
